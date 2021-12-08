1cf8fb9db54806df72efd6c05294601fdcae6cb1
Bhagyasri Kota <gupsb@nat-wireless-guest-reg-153-53.bu.edu>
2021-12-08 13:00:33 -0500

    merging changes

--- src/BankConstants.java
+++ src/BankConstants.java
@@ -1,9 +1,13 @@
 
 public class BankConstants {
-	private static double openAccountFee = 100.00;
+	private static double openAccountFee = 10.00;
+	private static double closeAccountFee = 20.00;
 	private static double minOpenAccountBalance = 100.00;
 	private static double minOpenSecuritiesAccountBalance = 5000.00;
-	
+	private static double savingsInterestPercentage = 0.03;
+	private static double loanInterestPercentage = 0.1;
+	private static double withDrawFeePercentage = 0.01;
+
 	
 	public static double getOpenAccountFee() {
 		return openAccountFee;
@@ -15,6 +19,22 @@ public class BankConstants {
 		}
 		openAccountFee = fee;
 		return true;
+	}
+	
+	public static double getCloseAccountFee() {
+		return closeAccountFee;
+	}
+	
+	public static double getSavingsInterestPercentage() {
+		return savingsInterestPercentage;
+	}
+	
+	public static double getLoanInterestPercentage() {
+		return loanInterestPercentage;
+	}
+	
+	public static double getWithDrawFeePercentage() {
+		return withDrawFeePercentage;
 	}
 	
 	public static double getMinOpenAccountBalance() {
